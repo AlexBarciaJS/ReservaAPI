8f4939f69ea45e2ab7196380f06cff43f1aa7648
AlexBarciaJS <guillermoalexisbarcia@gmail.com>
2025-06-16 11:15:07 -0500

    fix: admin routes

--- src/Controller/SpaceController.php
+++ src/Controller/SpaceController.php
@@ -14,7 +14,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_ADMIN')]
 #[Route('/api/spaces')]
 #[OA\Tag(name: 'Spaces')]
 class SpaceController extends AbstractController
